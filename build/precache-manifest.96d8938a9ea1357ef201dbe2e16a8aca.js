676bc0fbeaac775bb9929328192e86f413b760da
james <jamesakpan016@gmail.com>
2020-06-30 18:01:21 +0100

    remove build folder from being ignored

---
+++ build/precache-manifest.96d8938a9ea1357ef201dbe2e16a8aca.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89452c61d7a020a5c314cda0e055b856",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5bfd25c3e7546170c50",
+    "url": "/static/css/main.8200c858.chunk.css"
+  },
+  {
+    "revision": "b4886a9af30b6d6f73be",
+    "url": "/static/js/2.c50314ca.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/static/js/2.c50314ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5bfd25c3e7546170c50",
+    "url": "/static/js/main.8f32c450.chunk.js"
+  },
+  {
+    "revision": "31f87d868a201e61bbc1",
+    "url": "/static/js/runtime-main.f9bd1330.js"
+  },
+  {
+    "revision": "10a5b071548379871a331ddae24e289f",
+    "url": "/static/media/check.10a5b071.svg"
+  },
+  {
+    "revision": "5f127effae3b81ce1e99feb9a0d8defa",
+    "url": "/static/media/colorgoInIcon.5f127eff.svg"
+  },
+  {
+    "revision": "7c9993e17c1c8bc0232d7381a92f5920",
+    "url": "/static/media/delete.7c9993e1.svg"
+  },
+  {
+    "revision": "8d40c87c01aa80958c5e235e454a0cfe",
+    "url": "/static/media/purplegoIcon.8d40c87c.svg"
+  },
+  {
+    "revision": "77b7e61b5d45723371aba1d4462f5b26",
+    "url": "/static/media/undoIcon.77b7e61b.svg"
+  }
+]);
